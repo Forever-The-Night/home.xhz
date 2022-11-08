9b173fe12e8790a5bb4a2abfafda90fa9719a55c
Forever-The-Night <1617569457@qq.com>
2022-11-08 15:12:52 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,378 @@
+$(document).ready(function () {
+  // typing animation
+  (function ($) {
+    $.fn.writeText = function (content) {
+      var contentArray = content.split(""),
+        current = 0,
+        elem = this;
+      setInterval(function () {
+        if (current < contentArray.length) {
+          elem.text(elem.text() + contentArray[current++]);
+        }
+      }, 80);
+    };
+  })(jQuery);
+
+  // input text for typing animation
+  $("#holder").writeText("WEB DESIGNER + FRONT-END DEVELOPER");
+
+  // initialize wow.js
+  new WOW().init();
+
+  // Push the body and the nav over by 285px over
+  var main = function () {
+    $(".fa-bars").click(function () {
+      $(".nav-screen").animate(
+        {
+          right: "0px"
+        },
+        200
+      );
+
+      $("body").animate(
+        {
+          right: "285px"
+        },
+        200
+      );
+    });
+
+    // Then push them back */
+    $(".fa-times").click(function () {
+      $(".nav-screen").animate(
+        {
+          right: "-285px"
+        },
+        200
+      );
+
+      $("body").animate(
+        {
+          right: "0px"
+        },
+        200
+      );
+    });
+
+    $(".nav-links a").click(function () {
+      $(".nav-screen").animate(
+        {
+          right: "-285px"
+        },
+        500
+      );
+
+      $("body").animate(
+        {
+          right: "0px"
+        },
+        500
+      );
+    });
+  };
+
+  $(document).ready(main);
+
+  // initiate full page scroll
+
+  $("#fullpage").fullpage({
+    scrollBar: true,
+    responsiveWidth: 400,
+    navigation: true,
+    navigationTooltips: ["home", "about", "portfolio", "contact", "connect"],
+    anchors: ["home", "about", "portfolio", "contact", "connect"],
+    menu: "#myMenu",
+    fitToSection: false,
+
+    afterLoad: function (anchorLink, index) {
+      var loadedSection = $(this);
+
+      //using index
+      if (index == 1) {
+        /* add opacity to arrow */
+        $(".fa-chevron-down").each(function () {
+          $(this).css("opacity", "1");
+        });
+        $(".header-links a").each(function () {
+          $(this).css("color", "white");
+        });
+        $(".header-links").css("background-color", "transparent");
+      } else if (index != 1) {
+        $(".header-links a").each(function () {
+          $(this).css("color", "black");
+        });
+        $(".header-links").css("background-color", "white");
+      }
+
+      //using index
+      if (index == 2) {
+        /* animate skill bars */
+        $(".skillbar").each(function () {
+          $(this)
+            .find(".skillbar-bar")
+            .animate(
+              {
+                width: $(this).attr("data-percent")
+              },
+              2500
+            );
+        });
+      }
+    }
+  });
+
+  // move section down one
+  $(document).on("click", "#moveDown", function () {
+    $.fn.fullpage.moveSectionDown();
+  });
+
+  // fullpage.js link navigation
+  $(document).on("click", "#skills", function () {
+    $.fn.fullpage.moveTo(2);
+  });
+
+  $(document).on("click", "#projects", function () {
+    $.fn.fullpage.moveTo(3);
+  });
+
+  $(document).on("click", "#contact", function () {
+    $.fn.fullpage.moveTo(4);
+  });
+
+  // smooth scrolling
+  $(function () {
+    $("a[href*=#]:not([href=#])").click(function () {
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        if (target.length) {
+          $("html,body").animate(
+            {
+              scrollTop: target.offset().top
+            },
+            700
+          );
+          return false;
+        }
+      }
+    });
+  });
+
+  //ajax form
+  $(function () {
+    // Get the form.
+    var form = $("#ajax-contact");
+
+    // Get the messages div.
+    var formMessages = $("#form-messages");
+
+    // Set up an event listener for the contact form.
+    $(form).submit(function (e) {
+      // Stop the browser from submitting the form.
+      e.preventDefault();
+
+      // Serialize the form data.
+      var formData = $(form).serialize();
+
+      // Submit the form using AJAX.
+      $.ajax({
+        type: "POST",
+        url: $(form).attr("action"),
+        data: formData
+      })
+        .done(function (response) {
+          // Make sure that the formMessages div has the 'success' class.
+          $(formMessages).removeClass("error");
+          $(formMessages).addClass("success");
+
+          // Set the message text.
+          $(formMessages).text(response);
+
+          // Clear the form.
+          $("#name").val("");
+          $("#email").val("");
+          $("#message").val("");
+        })
+        .fail(function (data) {
+          // Make sure that the formMessages div has the 'error' class.
+          $(formMessages).removeClass("success");
+          $(formMessages).addClass("error");
+
+          // Set the message text.
+          if (data.responseText !== "") {
+            $(formMessages).text(data.responseText);
+          } else {
+            $(formMessages).text(
+              "Oops! An error occured and your message could not be sent."
+            );
+          }
+        });
+    });
+  });
+});
+const $window = $(window);
+const $body = $('body');
+
+class Slideshow {
+  constructor(userOptions = {}) {
+    const defaultOptions = {
+      $el: $('.slideshow'),
+      showArrows: false,
+      showPagination: true,
+      duration: 10000,
+      autoplay: true };
+
+
+    let options = Object.assign({}, defaultOptions, userOptions);
+
+    this.$el = options.$el;
+    this.maxSlide = this.$el.find($('.js-slider-home-slide')).length;
+    this.showArrows = this.maxSlide > 1 ? options.showArrows : false;
+    this.showPagination = options.showPagination;
+    this.currentSlide = 1;
+    this.isAnimating = false;
+    this.animationDuration = 1200;
+    this.autoplaySpeed = options.duration;
+    this.interval;
+    this.$controls = this.$el.find('.js-slider-home-button');
+    this.autoplay = this.maxSlide > 1 ? options.autoplay : false;
+
+    this.$el.on('click', '.js-slider-home-next', event => this.nextSlide());
+    this.$el.on('click', '.js-slider-home-prev', event => this.prevSlide());
+    this.$el.on('click', '.js-pagination-item', event => {
+      if (!this.isAnimating) {
+        this.preventClick();
+        this.goToSlide(event.target.dataset.slide);
+      }
+    });
+
+    this.init();
+  }
+
+  init() {
+    this.goToSlide(1);
+    if (this.autoplay) {
+      this.startAutoplay();
+    }
+
+    if (this.showPagination) {
+      let paginationNumber = this.maxSlide;
+      let pagination = '<div class="pagination"><div class="container">';
+
+      for (let i = 0; i < this.maxSlide; i++) {
+        let item = `<span class="pagination__item js-pagination-item ${i === 0 ? 'is-current' : ''}" data-slide=${i + 1}>${i + 1}</span>`;
+        pagination = pagination + item;
+      }
+
+      pagination = pagination + '</div></div>';
+
+      this.$el.append(pagination);
+    }
+  }
+
+  preventClick() {
+    this.isAnimating = true;
+    this.$controls.prop('disabled', true);
+    clearInterval(this.interval);
+
+    setTimeout(() => {
+      this.isAnimating = false;
+      this.$controls.prop('disabled', false);
+      if (this.autoplay) {
+        this.startAutoplay();
+      }
+    }, this.animationDuration);
+  }
+
+  goToSlide(index) {
+    this.currentSlide = parseInt(index);
+
+    if (this.currentSlide > this.maxSlide) {
+      this.currentSlide = 1;
+    }
+
+    if (this.currentSlide === 0) {
+      this.currentSlide = this.maxSlide;
+    }
+
+    const newCurrent = this.$el.find('.js-slider-home-slide[data-slide="' + this.currentSlide + '"]');
+    const newPrev = this.currentSlide === 1 ? this.$el.find('.js-slider-home-slide').last() : newCurrent.prev('.js-slider-home-slide');
+    const newNext = this.currentSlide === this.maxSlide ? this.$el.find('.js-slider-home-slide').first() : newCurrent.next('.js-slider-home-slide');
+
+    this.$el.find('.js-slider-home-slide').removeClass('is-prev is-next is-current');
+    this.$el.find('.js-pagination-item').removeClass('is-current');
+
+    if (this.maxSlide > 1) {
+      newPrev.addClass('is-prev');
+      newNext.addClass('is-next');
+    }
+
+    newCurrent.addClass('is-current');
+    this.$el.find('.js-pagination-item[data-slide="' + this.currentSlide + '"]').addClass('is-current');
+  }
+
+  nextSlide() {
+    this.preventClick();
+    this.goToSlide(this.currentSlide + 1);
+  }
+
+  prevSlide() {
+    this.preventClick();
+    this.goToSlide(this.currentSlide - 1);
+  }
+
+  startAutoplay() {
+    this.interval = setInterval(() => {
+      if (!this.isAnimating) {
+        this.nextSlide();
+      }
+    }, this.autoplaySpeed);
+  }
+
+  destroy() {
+    this.$el.off();
+  }}
+
+
+(function () {
+  let loaded = false;
+  let maxLoad = 3000;
+
+  function load() {
+    const options = {
+      showPagination: true };
+
+
+    let slideShow = new Slideshow(options);
+  }
+
+  function addLoadClass() {
+    $body.addClass('is-loaded');
+
+    setTimeout(function () {
+      $body.addClass('is-animated');
+    }, 600);
+  }
+
+  $window.on('load', function () {
+    if (!loaded) {
+      loaded = true;
+      load();
+    }
+  });
+
+  setTimeout(function () {
+    if (!loaded) {
+      loaded = true;
+      load();
+    }
+  }, maxLoad);
+
+  addLoadClass();
+})();
